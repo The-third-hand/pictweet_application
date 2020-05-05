8a0105a02f7a59507653fcb24f22e26b25974288
EC2 Default User <ec2-user@ip-172-31-38-198.ap-northeast-1.compute.internal>
2020-05-05 12:05:41 +0000

    ツイートの投稿で動くアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'tweets' => 'tweets#index'
-    
+    get 'tweets'      => 'tweets#index'
+    ger 'tweets/new'  => 'tweets#new'
 end
