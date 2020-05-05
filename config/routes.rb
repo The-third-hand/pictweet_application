341f9a2ea365a2128b8a56b2e498dc26d592d6ec
EC2 Default User <ec2-user@ip-172-31-38-198.ap-northeast-1.compute.internal>
2020-05-05 12:21:28 +0000

    ツイート保存のアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'tweets'      => 'tweets#index'
-    ger 'tweets/new'  => 'tweets#new'
+    get 'tweets/new'  => 'tweets#new'
+    post 'tweets'     => 'tweets#create'
 end
